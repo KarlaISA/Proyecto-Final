3f5da7cfaf545493034b4ab7d45d4865f0ce3c73
lccastro9 <luisk9-_@hotmail.com>
2017-06-30 15:22:54 -0500

    Update Proyecto final.py

--- Proyecto final.py
+++ Proyecto final.py
@@ -40,3 +40,49 @@ def nom_files(path_carpeta):
         if os.path.isdir(path_carpeta + "/"+i)==False:
             nom_img.append(i)
     return nom_img
+
+###########################################################
+##esto es lo que agregue nuevo XD no mas
+#aqui es la interfaz del programa
+from Tkinter import *
+import Image, ImageTk, os
+
+class directorio:
+ def __init__(self, master=None):
+   self.master = master
+   self.frame = Frame(master, width=400, height=450, bg='blue50',
+     relief=RAISED, bd=4)
+
+   self.lbl = Label(self.frame)
+   self.lbl.place(relx=0.5, rely=0.48, anchor=CENTER)
+   
+   self.images = []
+   images = os.listdir("images")
+
+   xpos = 0.05
+   for i in range(10):
+     Button(self.frame, text='%d'%(i+1), bg='gray10',
+     fg='white', command=lambda s=self, img=i: \
+     s.getImg(img)).place(relx=xpos, rely=0.99, anchor=S)
+     xpos = xpos + 0.08
+     self.images.append(images[i])
+
+   Button(self.frame, text='Done', command=self.exit,
+     bg='red', fg='yellow').place(relx=0.99, rely=0.99, anchor=SE)
+   self.frame.pack()
+   self.getImg(0)
+
+ def getImg(self, img):
+   self.masterImg = Image.open(os.path.join("images",
+   		self.images[img]))
+   self.masterImg.thumbnail((400, 400))
+   self.img = ImageTk.PhotoImage(self.masterImg)
+   self.lbl['image'] = self.img
+
+ def exit(self):
+   self.master.destroy()
+
+root = Tk()
+root.title('Album de Imagenes')
+scrapbook = directorio(root)
+root.mainloop()
