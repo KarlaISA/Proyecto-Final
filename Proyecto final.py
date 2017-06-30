d0e27c15cdd2698f0311fc1c22c841ba2d4311cc
Karla ISA <karliz.0935377k@gmail.com>
2017-06-30 11:59:46 -0500

    Update Proyecto final.py

--- Proyecto final.py
+++ Proyecto final.py
@@ -2,7 +2,7 @@
 #Karla Ivonne Serrano Arevalo
 #Luis Carlos Castro
 
-#aqui inicializamos las variables , nombre , etiqueta
+
 class Imagen:
     def __init__(self,nombre, etiqueta):
         self.nombre = nombre 
@@ -19,3 +19,24 @@ class Imagen:
        
     def mostrar_imagen(self):
        return Imagen
+
+
+#Comprobamos si carpetas dentro de la carpeta:
+def comprobar_carpeta(path_carpeta):
+  path_carpetas = []
+  path_carpetas.append(path_carpeta)
+  elementos_carpeta = os.listdir(path_carpeta)
+  for i in elementos_carpeta:
+    if os.path.isdir(path_carpeta+"/"+i)==True:
+        path_carpetas.append(path_carpeta+"/"+i)
+  return path_carpeta
+
+
+#
+def nom_files(path_carpeta):
+    nom_img_temp = os.listdir(path_carpeta)
+    nom_img = []
+    for i in nom_img_temp:
+        if os.path.isdir(path_carpeta + "/"+i)==False:
+            nom_img.append(i)
+    return nom_img
