79c4d067b3410c4fb6e65d9e7ab447dd2fa4462d
lccastro9 <luisk9-_@hotmail.com>
2017-06-30 11:45:56 -0500

    Update Proyecto final.py

--- Proyecto final.py
+++ Proyecto final.py
@@ -1,4 +1,19 @@
 #Proyecto Final
 #Karla Ivonne Serrano Arevalo
 #Luis Carlos Castro
- pikachu
+class Imagen:
+    def __init__(self,nombre, etiqueta):
+        self.nombre = nombre 
+        self.etiqueta = etiqueta
+    
+    def setNombre(self, nombre):
+        self.nombre = nombre 
+        
+    def getNombre(self):
+        return self.nombre
+    
+    def setEtiqueta(self, etiqueta):
+        self.etiqueta = etiqueta
+       
+    def mostrar_imagen(self):
+       return Imagen
